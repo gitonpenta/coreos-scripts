838910455053d3b61d06156f45bce80ae11b72af
CoreOS Admin <core@ip-172-31-7-206.eu-central-1.compute.internal>
2016-03-02 11:24:19 +0000

    fix start-container e http.service

--- start-container.sh
+++ start-container.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-service rabbitmq-server start > /tmp/rabbit.out 2> /tmp/rabbit.err
-/usr/sbin/rabbitmqctl add_user gmzeuboy gmzeupass >> /tmp/rabbit.out 2>> /tmp/rabbit.err
-/usr/sbin/rabbitmqctl add_vhost gmzeuboy >> /tmp/rabbit.out 2>> /tmp/rabbit.err
-/usr/sbin/rabbitmqctl set_permissions -p gmzeuboy gmzeuboy '.*' '.*' '.*' >> /tmp/rabbit.out 2>> /tmp/rabbit.err
+service rabbitmq-server start 
+/usr/sbin/rabbitmqctl add_user gmzeuboy gmzeupass 
+/usr/sbin/rabbitmqctl add_vhost gmzeuboy
+/usr/sbin/rabbitmqctl set_permissions -p gmzeuboy gmzeuboy '.*' '.*' '.*'
 cd /root/AetDjango && python manage.py runserver 0.0.0.0:8000 &
 cd /root/AetDjango && celery worker -A aeternuum.celery -B -l info &
 cd
